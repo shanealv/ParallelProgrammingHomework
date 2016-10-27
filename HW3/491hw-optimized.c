c86b27629a72610e950646ceae8b41410d6e7b7a
shanealv <shane.alv@gmail.com>
2016-10-26 21:58:12 -0700

    Finished Code, need to test on remote machine

--- HW3/491hw-optimized.c
+++ HW3/491hw-optimized.c
@@ -11,7 +11,7 @@
 double xold[(N + 2)][(N + 2)];
 double xnew[(N + 2)][(N + 2)];
 
-main(int argc, char * argv[])
+int main(int argc, char * argv[])
 {
 	double rtclock();
 	double clkbegin, clkend, t;
@@ -35,40 +35,67 @@ main(int argc, char * argv[])
 		for (j = 1; j < N + 1; j++) xold[i][j] = 0;
 
 	clkbegin = rtclock();
-
+	int done = 0;
+	omp_set_dynamic( 0 );
+	omp_set_num_threads(4);
+	int num_threads;
+	
 	for (iter = 0; iter < maxiter; iter++)
 	{
-
 		maxdiff = 0;
-		for (i = 1; i < N + 1; i++)
+	#pragma omp parallel private(i,j,thisdiff)
+	{
+		#pragma omp single
+		num_threads = omp_get_num_threads();
+		#pragma omp barrier
+		
+		int id = omp_get_thread_num();
+		int start = (N / num_threads) * id + 1; 
+		int end = (N / num_threads) * (id + 1) + 1;
+		if (id == num_threads - 1) end = N + 1;
+		
+		double diff = 0, temp;
+		for (i = start; i < end; i++)
 			for (j = 1; j < N + 1; j++)
-			{
-				xnew[i][j] = 0.25*(xold[i - 1][j] + xold[i + 1][j] + xold[i][j - 1] + xold[i][j + 1]);
-				if ((thisdiff = fabs(xnew[i][j] - xold[i][j])) > maxdiff) maxdiff = thisdiff;
+			{	
+				temp =  0.25*(xold[i - 1][j] + xold[i + 1][j] + xold[i][j - 1] + xold[i][j + 1]);
+				xnew[i][j] = temp;
+				if ((thisdiff = fabs(temp - xold[i][j])) > diff) 
+					diff = thisdiff;
 			}
+		
+		#pragma omp critical
+		if (diff > maxdiff)
+			maxdiff = diff;
+		#pragma omp barrier
+		
+		#pragma omp single
 		if (maxdiff < epsilon) {
 			clkend = rtclock();
+			done = 1;
 			printf("Solution converged in  %d iterations\n", iter + 1);
 			printf("Solution at center of grid : %f\n", xnew[(N + 1) / 2][(N + 1) / 2]);
 			t = clkend - clkbegin;
-			printf("Base-Jacobi: %.1f MFLOPS; Time = %.3f sec; \n",
-				4.0*N*N*(iter + 1) / t / 1000000, t);
-			break;
+			printf("Base-Jacobi: %.1f MFLOPS; Time = %.3f sec; \n", 4.0*N*N*(iter + 1) / t / 1000000, t);
 		}
-
-		for (i = 1; i < N + 1; i++)
-			for (j = 1; j < N + 1; j++)
-				xold[i][j] = xnew[i][j];
+		
+		if (!done)
+			for (i = start; i < end; i++)
+				for (j = 1; j < N + 1; j++)
+					xold[i][j] = xnew[i][j];
+	}
+		if (done)
+			break;
 	}
 }
 
 
 double rtclock()
 {
-	struct timezone Tzp;
+	//struct timezone Tzp;
 	struct timeval Tp;
 	int stat;
-	stat = gettimeofday(&Tp, &Tzp);
+	stat = gettimeofday(&Tp, NULL);//&Tzp);
 	if (stat != 0) printf("Error return from gettimeofday: %d", stat);
 	return(Tp.tv_sec + Tp.tv_usec*1.0e-6);
 }
